7e47f5992df9d0bed65d48e9f6caae66df2a40aa
sahil-darj <darjisahil46@gmail.com>
2026-02-24 10:44:29 +0530

    naroda group

--- admin2/api/debug_projects.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'config.php';
-$stmt = $pdo->query("SELECT id, name, detail_url FROM projects ORDER BY id DESC");
-$projects = $stmt->fetchAll(PDO::FETCH_ASSOC);
-print_r($projects);
-?>
--- admin2/api/list_types.php
+++
@@ -1,7 +0,0 @@
-<?php
-include 'config.php';
-$stmt = $pdo->query("SELECT DISTINCT type FROM projects");
-while($row = $stmt->fetch()) {
-    echo $row['type'] . "\n";
-}
-?>
